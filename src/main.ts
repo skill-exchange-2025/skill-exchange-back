a4762d69ca176b40ec6e3d59ecac0bed54edecfd
oookbaaa <okba.bedhiafi@esprit.tn>
2025-02-17 11:53:56 +0100

    fix main

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-<<<<<<< Updated upstream
-=======
-
-
- // Enable CORS
- app.enableCors({
-  origin: 'http://localhost:5173', 
-  credentials: true,
-});
-
-
-  
->>>>>>> Stashed changes
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 5000);
 
   app.setGlobalPrefix('api');
